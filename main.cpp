78b3b319717647aadc43263d7872a1187252b500
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-11-17 16:41:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   float a=0, b=0, x=0;
+    int opzione=0;
+    cin >> a >> b >> opzione;
+    switch(opzione) {
+        case 1:
+            x = (a * b) / 2;
+            cout << x << endl;
+            break;
+        case 2:
+            x = a * a;
+            cout << x << endl;
+            break;
+        case 3:
+            x = a * b;
+            cout << x << endl;
+            break;
+        default:
+            cout << "istruzione non valida" << endl;
+  }
    return 0;
 }
